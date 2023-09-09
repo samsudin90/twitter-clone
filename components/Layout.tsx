8ed3c800a7da3da322c243a506ceac8e76807cd2
Ilyas Samsudin <ilyassmsdn@gmail.com>
2023-09-09 23:22:45 +0700

    fix sized

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <div className="h-screen bg-black">
-            <div className="container h-full mx-auto xl:px-32 max-w-6xl">
+            <div className="container h-full mx-auto  max-w-7xl">
                 <div className="grid grid-cols-4 h-full">
                     <Sidebar/>
                     <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
